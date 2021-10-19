3e61e8f4996184b197977691338506d7a9bd361b
StevePro7 <steveprozune@hotmail.com>
2021-10-19 10:26:50 +0100

    no message

--- dev/main.c
+++ dev/main.c
@@ -15,6 +15,27 @@ void init() {
 	msc_rules_add_file( rules, "rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", &error );
 	return;
 }
+
+int process( const char *uri )
+{
+	////Transaction *transaction = NULL;
+	////transaction = msc_new_transaction( modsec, rules, NULL );
+	////msc_process_connection( transaction, "127.0.0.1", 80, "127.0.0.1", 80 );
+	////fprintf( stderr, "URI=%s\n", uri );
+	////msc_process_uri( transaction, uri, "CONNECT", "1.1" );
+	////msc_process_request_headers( transaction );
+	////msc_process_request_body( transaction );
+	////ModSecurityIntervention intervention;
+	////intervention.status = 200;
+	////intervention.url = NULL;
+	////intervention.log = NULL;
+	////intervention.disruptive = 0;
+	////int inter = msc_intervention( transaction, &intervention );
+	int inter = 1;
+	////fprintf( stderr, "intervention=%i\n", inter );
+	return inter;
+}
+
 void cleanup() {
 	msc_cleanup( modsec );
 	msc_rules_cleanup( rules );
@@ -23,6 +44,11 @@ void cleanup() {
 int main( int argc, char **argv )
 {
 	init();
+	int inter = process( "/test" );
+	if( inter > 0 )
+	{
+		printf( "Blocked!\n" );
+	}
 	cleanup();
 
 	return 0;
