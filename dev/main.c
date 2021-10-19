440718886b46aaf83cf6b43ecdad1baef361e121
StevePro7 <steveprozune@hotmail.com>
2021-10-19 10:24:48 +0100

    no message

--- dev/main.c
+++ dev/main.c
@@ -1,20 +1,29 @@
 #include "main.h"
 
-char main_rule_uri[] = "basic_rules.conf";
+ModSecurity *modsec = NULL;
+RulesSet *rules = NULL;
+
+void init() {
+	if( modsec != NULL ) {
+		return;
+	}
+	modsec = msc_init();
+	rules = msc_create_rules_set();
+	const char *error = NULL;
+	msc_rules_add_file( rules, "modsecdefault.conf", &error );
+	msc_rules_add_file( rules, "crs-setup.conf", &error );
+	msc_rules_add_file( rules, "rules/REQUEST-942-APPLICATION-ATTACK-SQLI.conf", &error );
+	return;
+}
+void cleanup() {
+	msc_cleanup( modsec );
+	msc_rules_cleanup( rules );
+}
 
 int main( int argc, char **argv )
 {
-	ModSecurity *modsec = NULL;
-	RulesSet *rules = NULL;
-
-	modsec = msc_init();
-	rules = msc_create_rules_set();
-	
-	int age = modsec->GetAge();
-	bar();
-
-	msc_cleanup( modsec );
-	msc_rules_cleanup( rules );
+	init();
+	cleanup();
 
 	return 0;
 }
