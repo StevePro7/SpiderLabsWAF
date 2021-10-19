588a837537f01dc724f3b90e0b4ec5620f190086
StevePro7 <steveprozune@hotmail.com>
2021-10-19 10:18:28 +0100

    no message

---
+++ dev/headers/modsecurity/rules_set.h
@@ -0,0 +1,15 @@
+#ifndef HEADERS_MODSECURITY_RULES_SET_H_
+#define HEADERS_MODSECURITY_RULES_SET_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	void foo( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // HEADERS_MODSECURITY_RULES_SET_H_
---
+++ dev/src/rules_set.cc
@@ -0,0 +1,7 @@
+#include "modsecurity/rules_set.h"
+#include "modsecurity/modsecurity.h"
+#include "modsecurity/transaction.h"
+
+extern "C" void foo( void )
+{
+}
